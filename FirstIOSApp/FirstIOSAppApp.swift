3a8fa19a5cf4ba76a80312f8449d3902b99959fd
bryanbator <bryanbator@icloud.com>
2025-07-21 19:09:11 +0200

    Initial Commit

---
+++ FirstIOSApp/FirstIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstIOSAppApp.swift
+//  FirstIOSApp
+//
+//  Created by Bryan Bator on 21.07.25.
+//
+
+import SwiftUI
+
+@main
+struct FirstIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirstIOSAppTests/FirstIOSAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstIOSAppTests.swift
+//  FirstIOSAppTests
+//
+//  Created by Bryan Bator on 21.07.25.
+//
+
+import Testing
+@testable import FirstIOSApp
+
+struct FirstIOSAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
